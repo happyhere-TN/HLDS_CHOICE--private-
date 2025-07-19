eded4f28a9aa6db1ae1a8694d539157a11bcebbf
happyhere-TN <odhahri855@gmail.com>
2025-07-19 20:46:15 +0100

    Add files via upload

---
+++ xashds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+alias login="sudo -i"
+login
+
+#fast shell's
+alias start-cstrike="cd && cd /home/xash/ && chmod +x * && ./xash -game cstrike"
+alias start-halflife="cd && cd /home/xash/ && chmod +x * && ./xash -game valve"
+alias cs-server-edit="cd && cd /home/xash/ && chmod +x * && cd csrike && nano server.cfg"
+alias hl-serveredit="cd && cd /home/xash/ && chmod +x * && cd valve && nano server.cfg"
+
+#select-number
+
+echo "tipe the name u want:
+start-cstrike
+start-hl
+cs-serveredit
+hl-serveredit"
+
