e1831a5c9c3272f042a336ae7f89aef9c0f9e70b
happyhere-TN <odhahri855@gmail.com>
2025-07-19 20:27:33 +0100

    Update choiceengine.sh

--- choiceengine.sh
+++ choiceengine.sh
@@ -6,10 +6,12 @@ alias installing="sleep 3 && clear && echo installation starting... "
 alias hlds="installing && cd && cd /home/xash && cd.. && mkdir hlds && cd xash && mv -rf * /home/hlds chmod +x * && amxhlds"
 alias amxhlds="cd && cd /home/hlds/cstrike && rm liblist.gam && cd .. && wget https://github.com/happyhere-TN/xashds_public_server-vps-_tutorial/raw/refs/heads/main/addons_for_xashds.zip && extamx"
 alias extamx="unzip addons_for_xashds.zip && rm addons_for_xashds.zip"
+alias hldsprof="cd && wget"
 alias xashds="installing && cd && cd /home/xash && rm filesystem_stdio.so && wget https://github.com/FWGS/xash3d-fwgs/releases/download/continuous-vbo-fix/xashds-linux-i386.tar.gz && xashlibs"
 alias xashlibs="tar -zxf xashds-linux-i386.tar.gz && cd xashds-linux-i386 && cp -rf * /home/xash && cd .. && rm xashds-linux-i386.tar.gz && rm -rf xashds-linux-i386 && chmod +x * && cd && cd /home/xash && amxxash"
 alias amxxash="cd && cd /home/xash/cstrike && rm liblist.gam && rm -rf addons && cd .. && wget https://github.com/happyhere-TN/xashds_public_server-vps-_tutorial/raw/refs/heads/main/addons_for_xashds.zip && extamxxash"
-alias extamxxash="unzip addons_for_xashds.zip && rm addons_for_xashds.zip"
+alias extamxxash="unzip addons_for_xashds.zip && rm addons_for_xashds.zip && xashprof"
+alias xashprof="cd && wget"
 
 #choce-game-engine
 echo "(7-7) tipe the name of game u want: 
