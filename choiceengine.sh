dbe6515b7301161f9fc648eee4ee573da392e035
happyhere-TN <odhahri855@gmail.com>
2025-07-19 20:21:34 +0100

    Add files via upload

--- choiceengine.sh
+++ choiceengine.sh
@@ -8,11 +8,10 @@ alias amxhlds="cd && cd /home/hlds/cstrike && rm liblist.gam && cd .. && wget ht
 alias extamx="unzip addons_for_xashds.zip && rm addons_for_xashds.zip"
 alias xashds="installing && cd && cd /home/xash && rm filesystem_stdio.so && wget https://github.com/FWGS/xash3d-fwgs/releases/download/continuous-vbo-fix/xashds-linux-i386.tar.gz && xashlibs"
 alias xashlibs="tar -zxf xashds-linux-i386.tar.gz && cd xashds-linux-i386 && cp -rf * /home/xash && cd .. && rm xashds-linux-i386.tar.gz && rm -rf xashds-linux-i386 && chmod +x * && cd && cd /home/xash && amxxash"
-alias amxxash="cd && cd /home/xash/cstrike && rm liblist.gam && rm -rf addons && cd .. && wget https://github.com/happyhere-TN/xashds_public_server-vps-_tutorial/raw/refs/heads/main/addons_for_xashds.zip
-"
+alias amxxash="cd && cd /home/xash/cstrike && rm liblist.gam && rm -rf addons && cd .. && wget https://github.com/happyhere-TN/xashds_public_server-vps-_tutorial/raw/refs/heads/main/addons_for_xashds.zip && extamxxash"
 alias extamxxash="unzip addons_for_xashds.zip && rm addons_for_xashds.zip"
 
 #choce-game-engine
-echo "tipe the name of game u want: 
+echo "(7-7) tipe the name of game u want: 
 xashds   or   hlds"
 
